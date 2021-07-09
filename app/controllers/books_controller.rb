04e59552b91694a4c843f8baf52e1a6ab457e75e
yamamoto-hirofumi <sun.lionkig.88@gmail.com>
2021-07-09 06:29:00 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,22 +18,32 @@ class BooksController < ApplicationController
   end
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(book.id)
+ 
+    end
+
   end
+
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+    if book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+    end
     redirect_to book_path(book[:id])
   end
+
   def destroy
     book = Book.find(params[:id])
-    book.destroy
+    if book.destroy
+      flash[:success] = "Book was successfully destroyed."
+    end
     redirect_to books_path
   end
 
   private
     def book_params
-    params.require(:book).permit(:title, :body)
+     params.require(:book).permit(:title, :body)
     end
 end
